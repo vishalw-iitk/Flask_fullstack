e29252542bd27300dfd525b2c90ddfbc7298395b
vishalw-iitk <vishalwghmre88@gmail.com>
2021-03-07 17:43:59 +0530

    local working sample added

--- stock/train.py
+++ stock/train.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 from flask import jsonify
 import os
+from os.path import exists
 import matplotlib
 matplotlib.use('Agg')
 np.random.seed(1337)
@@ -83,15 +84,19 @@ def process(df, db_connection):
     # original = original.loc[original['Date'] >= '2021-1-27']
     original = original.loc[original['Date'] >= '2019-12-1']
 
+    if exists("stock/open_stock_app/src/output.png"):
+        print("file existsssss")
+        os.remove("stock/open_stock_app/src/output.png")
+    else:
+        print("no fileeeeeee")
+
     ax = sns.lineplot(original['Date'], original['Open'])
     ax = sns.lineplot(df_forecast['Date'], df_forecast['Open'])
     fig = ax.get_figure()
     
-    if os.path.exists("open_stock_app/src/output.png"):
-        print("file existsssss")
-        os.remove("open_stock_app/src/output.png")
     
-    fig.savefig('open_stock_app/src/output.png')
+    
+    fig.savefig('stock/open_stock_app/src/output.png')
 
     df_forecast.to_sql(con=db_connection, name='forecast_table', if_exists='replace')
 
